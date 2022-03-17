e92048c8193cc47e7848ff93ddd80053c63db228
StellaChengZihan <101648531+StellaChengZihan@users.noreply.github.com>
2022-03-17 20:53:59 +0800

    Update and rename AppStoreGames.sql to AppStoreCatalog.sql

---
+++ sql/AppStoreCatalog.sql
@@ -0,0 +1,18 @@
+/***************************
+
+  Populate the games table
+
+****************************/
+
+INSERT INTO catalog VALUES ('227796', 'Pleasant Room along Bukit Timah', '50646', 'Bukit Timah', 'Private room', 80);
+INSERT INTO catalog VALUES ('367042', 'Ensuite Room (Room 1 & 2) near EXPO', '71609', 'Tampines', 'Private room', 179);
+INSERT INTO catalog VALUES ('1439258', 'Conveniently located City Room!(1,2,3,4,5,6,7,8)', '275343', 'Bukit Merah', 'Private room', 52);
+INSERT INTO catalog VALUES ('1759905', 'Double room in an Authentic Peranakan Shophouse', '355955', 'Geylang', 'Private room', 65);
+INSERT INTO catalog VALUES ('1975201', 'Heritage Apartment Room near Orchard', '395191', 'Bukit Timah', 'Private room', 80);
+INSERT INTO catalog VALUES ('2386154', 'Master Bedroom in Newly Built Flat', '481789', 'Tampines', 'Private room', 37);
+INSERT INTO catalog VALUES ('2996022', 'Boutique 1 bedroom entiere appart', '604966', 'Bukit Timah', 'Entire home/apt', 271);
+INSERT INTO catalog VALUES ('813925', 'Ensuite room, pool, near airport, expo and sutd.', '606784', 'Tampines', 'Private room', 83);
+INSERT INTO catalog VALUES ('3212572', 'BEST CITY LIVING WITH GA RESIDENCE', '642660', 'Rochor', 'Private room', 163);
+INSERT INTO catalog VALUES ('1289033', 'Quiet ensuite room in Holland Village', '696136', 'Bukit Timah', 'Private room', 100);
+
+
