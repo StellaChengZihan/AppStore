13c411e791dddca5a0792839569bc1e9fc0dc8cb
StellaChengZihan <101648531+StellaChengZihan@users.noreply.github.com>
2022-03-17 20:24:08 +0800

    Update AppStoreSchema.sql

--- sql/AppStoreSchema.sql
+++ sql/AppStoreSchema.sql
@@ -4,24 +4,22 @@
 
 ********************/
 
-CREATE TABLE IF NOT EXISTS customers (
- first_name VARCHAR(64) NOT NULL,
- last_name VARCHAR(64) NOT NULL,
- email VARCHAR(64) UNIQUE NOT NULL,
- dob DATE NOT NULL,
- since DATE NOT NULL,
- customerid VARCHAR(16) PRIMARY KEY,
- country VARCHAR(16) NOT NULL);
+CREATE TABLE IF NOT EXISTS accounts (
+ name VARCHAR(64) NOT NULL,
+ accountid VARCHAR(16) PRIMARY KEY,
+ ishost BOOLEAN NOT NULL);
 	
-CREATE TABLE IF NOT EXISTS games(
- name VARCHAR(32),
- version CHAR(3),
- price NUMERIC NOT NULL,
- PRIMARY KEY (name, version));
+CREATE TABLE IF NOT EXISTS catalog(
+ room_name VARCHAR(64) NOT NULL,
+ id VARCHAR(16) PRIMARY KEY,
+ neighborhood VARCHAR(64) NOT NULL,
+ room_type VARCHAR(64) NOT NULL,
+ price VARCHAR(64) NOT NULL;
   
- CREATE TABLE downloads(
+/* 
+CREATE TABLE downloads(
  customerid VARCHAR(16) REFERENCES customers(customerid) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
  name VARCHAR(32),
  version CHAR(3),
  PRIMARY KEY (customerid, name, version),
- FOREIGN KEY (name, version) REFERENCES games(name, version) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED);
+ FOREIGN KEY (name, version) REFERENCES games(name, version) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED);*/
