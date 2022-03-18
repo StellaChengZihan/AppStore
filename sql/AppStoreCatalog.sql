c097e716720a4dc6823807159a63529fb507a25f
StellaChengZihan <101648531+StellaChengZihan@users.noreply.github.com>
2022-03-18 14:37:45 +0800

    Update AppStoreCatalog.sql

--- sql/AppStoreCatalog.sql
+++ sql/AppStoreCatalog.sql
@@ -27,3 +27,24 @@ INSERT INTO catalog VALUES ('1521514', '5mins from Newton Train Station', '36914
 INSERT INTO catalog VALUES ('1521514', '5 mins walk from Newton MRT', '369145', 'Newton', 'Private room', 66);
 
 
+INSERT INTO catalog VALUES ('227796', 'Pleasant Room along Bukit Timah', '50646', 'Bukit Timah', 'Private room', 80);
+INSERT INTO catalog VALUES ('367042', 'Ensuite Room (Room 1 & 2) near EXPO', '71609', 'Tampines', 'Private room', 179);
+INSERT INTO catalog VALUES ('1439258', 'Conveniently located City Room!(1,2,3,4,5,6,7,8)', '275343', 'Bukit Merah', 'Private room', 52);
+INSERT INTO catalog VALUES ('1759905', 'Double room in an Authentic Peranakan Shophouse', '355955', 'Geylang', 'Private room', 65);
+INSERT INTO catalog VALUES ('1975201', 'Heritage Apartment Room near Orchard', '395191', 'Bukit Timah', 'Private room', 80);
+INSERT INTO catalog VALUES ('2386154', 'Master Bedroom in Newly Built Flat', '481789', 'Tampines', 'Private room', 37);
+INSERT INTO catalog VALUES ('2996022', 'Boutique 1 bedroom entiere appart', '604966', 'Bukit Timah', 'Entire home/apt', 271);
+INSERT INTO catalog VALUES ('813925', 'Ensuite room, pool, near airport, expo and sutd.', '606784', 'Tampines', 'Private room', 83);
+INSERT INTO catalog VALUES ('3212572', 'BEST CITY LIVING WITH GA RESIDENCE', '642660', 'Rochor', 'Private room', 163);
+INSERT INTO catalog VALUES ('1289033', 'Quiet ensuite room in Holland Village', '696136', 'Bukit Timah', 'Private room', 100);
+INSERT INTO catalog VALUES ('367042', 'B&B  Room 1 near Airport & EXPO', '71896', 'Tampines', 'Private room', 82);
+INSERT INTO catalog VALUES ('367042', 'Room 2-near Airport & EXPO', '71903', 'Tampines', 'Private room', 82);
+INSERT INTO catalog VALUES ('1439258', '15 mins to Outram MRT Single Room (B)', '275344', 'Bukit Merah', 'Private room', 40);
+INSERT INTO catalog VALUES ('1439258', '20 Mins to Sentosa @ Hilltop ! (8)', '324945', 'Bukit Merah', 'Private room', 49);
+INSERT INTO catalog VALUES ('1439258', 'Accomo@ REDHILL-INSEAD, NTU,NUS -Mu(D)', '330089', 'Bukit Merah', 'Private room', 59);
+INSERT INTO catalog VALUES ('2386154', '10 mins to Redhill MRT @ Mini Orange Room(5)', '330095', 'Tampines', 'Private room', 59);
+INSERT INTO catalog VALUES ('1439258', 'Awesome Yellow Room in the City! Near Orchard Rd!', '815685', 'Bukit Merah', 'Private room', 40);
+INSERT INTO catalog VALUES ('1439258', '10 Min Walk to HawParVilla MRT - INSEAD (A)', '2922184', 'Queenstown', 'Private room', 65);
+INSERT INTO catalog VALUES ('1521514', '5 mins walk from Newton subway', '294281', 'Newton', 'Private room', 72);
+INSERT INTO catalog VALUES ('1521514', '5mins from Newton Train Station', '369141', 'Newton', 'Private room', 69);
+INSERT INTO catalog VALUES ('1521514', '5 mins walk from Newton MRT', '369145', 'Newton', 'Private room', 66);
