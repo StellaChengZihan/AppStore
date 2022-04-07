a9cad0ec46d4458d402e9094092572dbf81e6149
lzj-0 <97489546+lzj-0@users.noreply.github.com>
2022-04-07 16:30:44 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -5,15 +5,17 @@ from django.db import connection
 def index(request):
     """Shows the main page"""
 
+    
     ## Delete listing
     if request.POST:
         if request.POST['action'] == 'delete':
             with connection.cursor() as cursor:
-                cursor.execute("DELETE FROM catalog WHERE id = %s", [request.POST['id']])
+                cursor.execute("DELETE FROM Catalog WHERE ID_place = %s", [request.POST['ID_place']])
+
 
     ## Use raw query to get all objects
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM catalog ORDER BY id")
+        cursor.execute("SELECT * FROM Catalog ORDER BY ID_place")
         listings = cursor.fetchall()
 
     result_dict = {'records': listings}
@@ -26,7 +28,7 @@ def view(request, id):
     
     ## Use raw query to get a listing
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM catalog WHERE id = %s", [id])
+        cursor.execute("SELECT * FROM Catalog c, Account a WHERE c.ID_account = a.ID AND ID_place = %s", [id])
         listing = cursor.fetchone()
     result_dict = {'cust': listing}
 
@@ -42,16 +44,20 @@ def add(request):
         ## Check if id is already in the table
         with connection.cursor() as cursor:
 
-            cursor.execute("SELECT * FROM catalog WHERE id = %s", [request.POST['id']])
+            cursor.execute("SELECT * FROM Catalog WHERE ID_account = %s", [request.POST['ID_account']])
             listing = cursor.fetchone()
             ## No account with same id
             if listing == None:
                 ##TODO: date validation
-                cursor.execute("INSERT INTO catalog VALUES (%s, %s, %s, %s, %s, %s)"
-                        , [request.POST['accountid'], request.POST['name'], request.POST['id'], request.POST['neighborhood'], request.POST['room_type'], request.POST['price']])
+                cursor.execute("INSERT INTO Catalog (ID_account, title, rating, country, city, price_per_night, type, address, guests, bedrooms, kitchen, parking, wifi, smoking_allowed, pets_allowed, air_conditioning, TV, washing_machine) VALUES (%s, %s, NULL, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                        , [request.POST['ID_account'], request.POST['title'],
+                           request.POST['country'], request.POST['city'], request.POST['price_per_night'], 
+                           request.POST['type'], request.POST['address'], request.POST['guests'], request.POST['bedrooms'], request.POST['kitchen'],
+                              request.POST['parking'], request.POST['wifi'], request.POST['smoking_allowed'], request.POST['pets_allowed'], request.POST['air_conditioning'],
+                              request.POST['TV'], request.POST['washing_machine']])
                 return redirect('index')    
             else:
-                status = 'Listing with ID %s already exists' % (request.POST['id'])
+                status = 'Listing of account %s with address %s already exists' % (request.POST['ID_account'], request.POST['address'])
 
 
     context['status'] = status
@@ -68,7 +74,7 @@ def edit(request, id):
 
     # fetch the object related to passed id
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM catalog WHERE id = %s", [id])
+        cursor.execute("SELECT * FROM Catalog WHERE ID_place = %s", [id])
         obj = cursor.fetchone()
 
     status = ''
@@ -77,10 +83,14 @@ def edit(request, id):
     if request.POST:
         ##TODO: date validation
         with connection.cursor() as cursor:
-            cursor.execute("UPDATE catalog SET accountid = %s, name = %s, id = %s, neighborhood = %s, room_type = %s, price = %s WHERE id = %s"
-                    , [request.POST['accountid'], request.POST['name'], request.POST['id'], request.POST['neighborhood'], request.POST['room_type'], request.POST['price'], id ])
+            cursor.execute("UPDATE Catalog SET title = %s, country = %s, \
+                         city = %s, price_per_night = %s, type = %s, address = %s, guests = %s, bedrooms = %s , kitchen = %s, parking = %s, wifi = %s \
+                          , smoking_allowed = %s, pets_allowed = %s, air_conditioning = %s, TV = %s, washing_machine = %s WHERE ID_place = %s"
+                    , [request.POST['title'], request.POST['country'], request.POST['city'], request.POST['price_per_night'], 
+                        request.POST['type'], request.POST['address'], request.POST['guests'], request.POST['bedrooms'], request.POST['kitchen'], request.POST['parking']
+                       , request.POST['wifi'], request.POST['smoking_allowed'], request.POST['pets_allowed'], request.POST['air_conditioning'], request.POST['TV'], request.POST['washing_machine'], id ])
             status = 'Listing edited successfully!'
-            cursor.execute("SELECT * FROM catalog WHERE id = %s", [id])
+            cursor.execute("SELECT * FROM Catalog WHERE ID_place = %s", [id])
             obj = cursor.fetchone()
 
 
